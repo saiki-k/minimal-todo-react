fe49d63bbb114dc9e797db1440737c3b37994bc6
Sai Kishore Komanduri <sai@hashnode.com>
2016-09-17 10:48:16 +0530

    Implement localStorage

--- src/lib/TodoDataInterface.js
+++ src/lib/TodoDataInterface.js
@@ -4,9 +4,31 @@ import * as _ from 'lodash';
 export default class TodoDataInterface {
     todos = [];
 
+    constructor() {
+        this.loadFromLocalStorage();
+    }
+
+    saveToLocalStorage() {
+        if (window.localStorage && this.todos) {
+            let jsonTodos = this.todos.map(todo => todo.serialize());
+            localStorage.setItem('minimalReactTodos', JSON.stringify(jsonTodos));
+        }
+    }
+
+    loadFromLocalStorage() {
+        if (window.localStorage) {
+            let todos = localStorage.getItem('minimalReactTodos');
+            if (todos) {
+                todos = JSON.parse(todos);
+                this.todos = todos.map(todo => Todo.deserialize(todo));
+            }
+        }
+    }
+
     addTodo(descriptionText) {
         const newTodo = new Todo(descriptionText);
         this.todos.push(newTodo);
+        this.saveToLocalStorage();
         return newTodo;
     }
 
@@ -15,6 +37,7 @@ export default class TodoDataInterface {
         if (todoIndex > -1) {
             this.todos[todoIndex].isDone = !this.todos[todoIndex].isDone
         }
+        this.saveToLocalStorage();
     }
 
     removeTodo(todoId) {
@@ -22,11 +45,10 @@ export default class TodoDataInterface {
         if (todoIndex > -1) {
             this.todos.splice(todoIndex, 1);
         }
+        this.saveToLocalStorage();
     }
 
     getAllTodos() {
-        return this.todos.map(function(todo) {
-            return todo;
-        });
+        return this.todos.map(todo => todo);
     }
 }
